36d7e48131454b095d4e338d0ceea78aa37d0fbd
SHUBHAM PATKAR <52624483+patkarshubham@users.noreply.github.com>
2022-07-14 11:40:20 +0530

    Add files via upload

---
+++ tech.php
@@ -0,0 +1,107 @@
+ <!-- Start Services Section -->
+ <div class="container text-center border-bottom" id="Services">
+  <h2>Our Services</h2>
+  <div class="row mt-4">
+   <div class="col-sm-4 mb-4">
+    <a href="#"><i class="fas fa-tv fa-8x text-success"></i></a>
+    <h4 class="mt-4">Electronic Appliances</h4>
+   </div>
+   <div class="col-sm-4 mb-4">
+    <a href="#"><i class="fas fa-sliders-h fa-8x text-primary"></i></a>
+    <h4 class="mt-4">Preventive Maintenance</h4>
+   </div>
+   <div class="col-sm-4 mb-4">
+    <a href="#"><i class="fas fa-cogs fa-8x text-info"></i></a>
+    <h4 class="mt-4">Fault Repair</h4>
+   </div>
+  </div>
+ </div> <!-- End Services Section Container -->
+
+
+ <!-- Start Happy Customer -->
+ <div class="jumbotron bg-danger">
+   <div class="container">
+    <h2 class="text-center text-white">Electronic Appliances</h2>
+    <div class="row mt-5">
+     <div class="col-lg-3 col-sm-6"> <!-- Start 1st Column -->
+      <div class="card shadow-lg mb-2">
+       <div class="card-body text-center">
+        <img src="images/avtar1.jpeg" class="img-fluid" style="border-radius:100px;" alt="avt1">
+        <h4 class="card-title">Laptop</h4>
+       </div>
+      </div>
+     </div> <!-- End 1st Column -->
+     <div class="col-lg-3 col-sm-6"> <!-- Start 2nd Column -->
+      <div class="card shadow-lg mb-2">
+       <div class="card-body text-center">
+        <img src="images/avtar2.jpeg" class="img-fluid" style="border-radius:100px;" alt="avt2">
+        <h4 class="card-title">Mobile</h4>
+        </div>
+      </div>
+     </div> <!-- End 2nd Column -->
+     <div class="col-lg-3 col-sm-6"> <!-- Start 3rd Column -->
+      <div class="card shadow-lg mb-2">
+       <div class="card-body text-center">
+        <img src="images/avtar3.jpeg" class="img-fluid" style="border-radius:100px;" alt="avt3">
+        <h4 class="card-title">Wifi</h4>
+        </div>
+      </div>
+     </div> <!-- End 3rd Column -->
+     <div class="col-lg-3 col-sm-6"> <!-- Start 4th Column -->
+      <div class="card shadow-lg mb-2">
+       <div class="card-body text-center">
+        <img src="images/avtar4.jpeg" class="img-fluid" style="border-radius:100px;" alt="avt4">
+        <h4 class="card-title">LCD</h4>
+        </div>
+      </div>
+     </div> <!-- End 4th Column -->
+     <div class="col-lg-3 col-sm-6"> <!-- Start 4th Column -->
+      <div class="card shadow-lg mb-2">
+       <div class="card-body text-center">
+        <img src="images/avtar4.jpeg" class="img-fluid" style="border-radius:100px;" alt="avt4">
+        <h4 class="card-title">Jyoti Sinha</h4>
+        <p class="card-text">It's a great era for any type technician services provider.</p>
+       </div>
+      </div>
+     </div> <!-- End 4th Column -->
+     <div class="col-lg-3 col-sm-6"> <!-- Start 4th Column -->
+      <div class="card shadow-lg mb-2">
+       <div class="card-body text-center">
+        <img src="images/avtar4.jpeg" class="img-fluid" style="border-radius:100px;" alt="avt4">
+        <h4 class="card-title">Jyoti Sinha</h4>
+        <p class="card-text">It's a great era for any type technician services provider.</p>
+       </div>
+      </div>
+     </div> <!-- End 4th Column -->
+     <div class="col-lg-3 col-sm-6"> <!-- Start 4th Column -->
+      <div class="card shadow-lg mb-2">
+       <div class="card-body text-center">
+        <img src="images/avtar4.jpeg" class="img-fluid" style="border-radius:100px;" alt="avt4">
+        <h4 class="card-title">Jyoti Sinha</h4>
+        <p class="card-text">It's a great era for any type technician services provider.</p>
+       </div>
+      </div>
+     </div> <!-- End 4th Column -->
+     <div class="col-lg-3 col-sm-6"> <!-- Start 4th Column -->
+      <div class="card shadow-lg mb-2">
+       <div class="card-body text-center">
+        <img src="images/avtar4.jpeg" class="img-fluid" style="border-radius:100px;" alt="avt4">
+        <h4 class="card-title">Jyoti Sinha</h4>
+        <p class="card-text">It's a great era for any type technician services provider.</p>
+       </div>
+      </div>
+     </div> <!-- End 4th Column -->
+     <div class="col-lg-3 col-sm-6"> <!-- Start 4th Column -->
+      <div class="card shadow-lg mb-2">
+       <div class="card-body text-center">
+        <img src="images/avtar4.jpeg" class="img-fluid" style="border-radius:100px;" alt="avt4">
+        <h4 class="card-title">Jyoti Sinha</h4>
+        <p class="card-text">It's a great era for any type technician services provider.</p>
+       </div>
+      </div>
+     </div> <!-- End 4th Column -->
+    </div>
+   </div>
+ </div> <!-- End Happy Customer -->
+
+ 
